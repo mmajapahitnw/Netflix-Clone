101404fe9fe128d85dbd4bb3f77d987cbc1ef229
Majapahit Wisisono <122672518+mmajapahitnw@users.noreply.github.com>
2025-07-23 12:38:42 +0700

    Created the homepage and managed to call tmdb api

---
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -0,0 +1,113 @@
+//
+//  HomeViewController.swift
+//  Netflix Clone
+//
+//  Created by Majapahit Wisisono on 22/07/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    let sectionTitles = [
+        "Trending Movies",
+        "Trending TV Series",
+        "Popular",
+        "Upcoming Movies",
+        "Top Rated"
+    ]
+    
+    private let homeFeedTable: UITableView = {
+        let table = UITableView(frame: .zero, style: .grouped)
+        table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
+        return table
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
+        
+        configureNavBar()
+        
+        let headerView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        homeFeedTable.tableHeaderView = headerView
+        
+        getTrendingMovies()
+    }
+    
+    private func configureNavBar() {
+        var image = UIImage(named: "netflixLogo")
+        image = image?.withRenderingMode(.alwaysOriginal)
+//        navigationItem.hidesBackButton = true
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        navigationItem.titleView?.tintColor = .label
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
+    }
+    
+    private func getTrendingMovies() {
+        APICaller.shared.getTrendingMovies { _ in
+            
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        // to hide nav bar when scrolling down
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else { return }
+        header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+        header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+        header.textLabel?.textColor = .white
+//        header.textLabel?.text = header.textLabel?.text?.lowercased()
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CollectionViewTableViewCell.identifier, for: indexPath) as? CollectionViewTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+}
---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,69 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Majapahit Wisisono on 23/07/25.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "7e177aa2ea0287b9d6ad6d423bfc9053"
+    static let READ_ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZTE3N2FhMmVhMDI4N2I5ZDZhZDZkNDIzYmZjOTA1MyIsIm5iZiI6MTc1MzIyNTcwNS4yMTI5OTk4LCJzdWIiOiI2ODgwMTllOTJkOTA1NTI1NmRkOTk4MDIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TLQwSYIW9fkBmYdfrRRaeyygUq1z0jTBH-Z2Gnke70g"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (String) -> Void) {
+        
+//        guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?language=en-US") else { return }
+                
+        // https://api.themoviedb.org/3/trending/movie/day?language=en-US
+        
+        
+
+        let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day")!
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true)!
+        let queryItems: [URLQueryItem] = [
+          URLQueryItem(name: "language", value: "en-US"),
+        ]
+        components.queryItems = components.queryItems.map { $0 + queryItems } ?? queryItems
+
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "GET"
+        request.timeoutInterval = 10
+        request.allHTTPHeaderFields = [
+          "accept": "application/json",
+          "Authorization": "Bearer \(Constants.READ_ACCESS_TOKEN)"
+        ]
+
+
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                print("error getting data")
+                return
+            }
+            
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+        
+        //        do {
+        //            let (data, _) = try await URLSession.shared.data(for: request)
+        //            print(String(decoding: data, as: UTF8.self))
+        //        } catch {
+        //            print(error.localizedDescription)
+        //        }
+        
+    }
+}
